9dc085ea3d9015dd8b64235990924a83c9e97d4c
Arilson F J Silva <arilsonsilva61@gmail.com>
2021-06-21 19:16:54 -0300

    implementacao do Main

--- src/Aplicações/MainTrabalhador.java
+++ src/Aplicações/MainTrabalhador.java
@@ -1,5 +1,8 @@
 package Aplicações;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author arils
@@ -8,6 +11,20 @@ public class MainTrabalhador {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.print("Infome o nome do departamento: ");
+        String nomeDepartamento = scn.nextLine();
+        System.out.println();
+        
+        System.out.println("Informe os dados do trabalhador");
+        System.out.print("Nome: ");
+        String nomeTrabalhador = scn.nextLine();
+        System.out.print("Nível: ");
+        String nivelTrabalhador = scn.nextLine();
+        System.out.print("Salário base: ");
+        double salarioBase = scn.nextDouble();
     }
 
 }
