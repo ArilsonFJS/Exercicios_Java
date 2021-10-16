bb287a9a6831953501679e0cdff2bd748330ec5b
ArilsonFJS <arilsonsilva61@gmail.com>
2021-10-16 09:03:25 -0300

    update

--- src/model/entidades/Parcelas.java
+++ src/model/entidades/Parcelas.java
@@ -7,8 +7,32 @@ import java.util.Date;
  * @author afrancelino
  */
 public class Parcelas {
-    
+
     private Date dataVencimento;
     private Double valor;
-    
+
+    public Parcelas() {
+    }
+
+    public Parcelas(Date dataVencimento, Double valor) {
+        this.dataVencimento = dataVencimento;
+        this.valor = valor;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
 }
