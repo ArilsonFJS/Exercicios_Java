665ac88e559c7af13138a2860d27627da1a8551e
ArilsonFJS <arilsonsilva61@gmail.com>
2021-10-16 09:01:50 -0300

    add entidades Parcelas e Contrato

---
+++ src/model/entidades/Parcelas.java
@@ -0,0 +1,14 @@
+package model.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author afrancelino
+ */
+public class Parcelas {
+    
+    private Date dataVencimento;
+    private Double valor;
+    
+}
