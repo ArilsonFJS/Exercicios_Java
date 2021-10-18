b161e2528e39fb86c19fe00361bfa8bcc83384b1
ArilsonFJS <arilsonsilva61@gmail.com>
2021-10-18 16:19:01 -0300

    update

--- src/model/entidades/RegistroEntrada.java
+++ src/model/entidades/RegistroEntrada.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.entidades;
+
+import java.util.Date;
+import java.util.Objects;
 
 /**
  *
  * @author afrancelino
  */
 public class RegistroEntrada {
+    
+    private String userName;
+    private Date moment;
+
+    public RegistroEntrada(String userName, Date moment) {
+        this.userName = userName;
+        this.moment = moment;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.userName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RegistroEntrada other = (RegistroEntrada) obj;
+        if (!Objects.equals(this.userName, other.userName)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
